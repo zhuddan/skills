58df169562e4018fb3f174be8b4ebed0c2d52b76
Dan <32126992+zhuddan@users.noreply.github.com>
2026-06-04 21:13:07 +0800

    Fix command to add shadcn skills with npx

--- install.sh
+++ install.sh
@@ -21,4 +21,4 @@ npx skills add https://github.com/github/awesome-copilot --skill git-commit -g
 
 npx skills add https://github.com/artwist-polyakov/polyakov-claude-skills --skill ssh-remote-connection -g
 
-skills add https://github.com/shadcn/ui --skill shadcn -g
+npx skills add https://github.com/shadcn/ui --skill shadcn -g
